497e9f20699090c2c00b60e61ca761032f1c60ba
WindRider7 <kuznet_o@epitech.eu>
2014-04-04 06:03:40 +0200

    ...

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@ void          start(std::string X, std::string Y, std::string lib)
 try
 {
   gl = p.graphInit(X, Y, lib); // start
-  c.start(gl); //             lvl down / end
+  c.start(gl); //                 lvl down / end
   delete gl;
 }
 catch (std::logic_error &error)
@@ -38,26 +38,26 @@ int           main(int argc, char const *argv[])
 {
   std::string helpArg = "--help";
 
-//  if (argc > 2)
-//  {
-//    if (helpArg.compare(argv[1]) == 0)
-//    {
-//      std::cout << " > Usage: './nibbler X Y lib_nibbler_***.so'" << std::endl
-//                << "Where : " << std::endl
-//                << "'***' - name of the GL" << std::endl
-//                << "X - game area width (px)" << std::endl
-//                << "Y - game area height (px)" << std::endl
-//                << "Game Area cannot exceed screen resolution" << std::endl
-//                << "Game Area cannot be less then 9 x 9" << std::endl;
-//      myExit(0);
-//    }
-//  }
-//  if (argc != 4)
-//  {
-//    std::cout << "Invalid number of arguments" << std::endl
-//              << "Use: './nibbler --help' for info" << std::endl;
-//    myExit(0);
-//  }
+  if (argc > 2)
+  {
+    if (helpArg.compare(argv[1]) == 0)
+    {
+      std::cout << " > Usage: './nibbler X Y lib_nibbler_***.so'" << std::endl
+                << "Where : " << std::endl
+                << "'***' - name of the GL" << std::endl
+                << "X - game area width (px)" << std::endl
+                << "Y - game area height (px)" << std::endl
+                << "Game Area cannot exceed screen resolution" << std::endl
+                << "Game Area cannot be less then 9 x 9" << std::endl;
+      myExit(0);
+    }
+  }
+  if (argc != 4)
+  {
+    std::cout << "Invalid number of arguments" << std::endl
+              << "Use: './nibbler --help' for info" << std::endl;
+    myExit(0);
+  }
   start(argv[1], argv[2], argv[3]); // start / lvl down
 	myExit(0);
 }
